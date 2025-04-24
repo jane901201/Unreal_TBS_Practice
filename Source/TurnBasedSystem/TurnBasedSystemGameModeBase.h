7fa5ff295e29741f6cb64f21efe19bddde59ce87
jane901201 <75302011+jane901201@users.noreply.github.com>
2025-04-25 04:46:31 +0900

    initinal commit.

---
+++ Source/TurnBasedSystem/TurnBasedSystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TurnBasedSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURNBASEDSYSTEM_API ATurnBasedSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
